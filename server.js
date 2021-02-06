80c886e0282a30e18769fd472e62fcf84ca8151b
MANISH KUMAR <36466808+manu05X@users.noreply.github.com>
2021-02-07 00:25:05 +0530

    return react app

--- server.js
+++ server.js
@@ -172,7 +172,14 @@ app.use("/payment", async (req, res) => {
     
 });
    
-
+if (process.env.NODE_ENV === "production")
+ {
+    app.use(express.static(path.join(__dirname, "client", "build")));
+    
+    app.get("/*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
+}
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Server is running at port 5000 .....");
